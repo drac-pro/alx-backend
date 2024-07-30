cf754a41c176957cc12e67296fd08eb0018d6a29
Darius Tohtin <dariustohtin@gmail.com>
2024-07-30 21:46:41 +0100

    pycodestyle edit

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,13 +5,16 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+
 class Config:
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @app.route('/')
 def index():
