34ccf3b4041a131c8b2d65298462a00d2c16c749
Darius Tohtin <dariustohtin@gmail.com>
2024-07-30 21:41:42 +0100

    task 1. Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""sets up a simple flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """index route"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
