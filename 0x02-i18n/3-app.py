168854c1312f458d8d042415eed96319881ced0a
Darius Tohtin <dariustohtin@gmail.com>
2024-07-30 22:36:25 +0100

    task 3. Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""sets up a simple flask app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """config for simple flask app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """determines the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """index route"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
