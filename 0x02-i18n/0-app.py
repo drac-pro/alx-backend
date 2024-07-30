442793cd54707c67b6aecbfced66fd93c15e2740
Darius Tohtin <dariustohtin@gmail.com>
2024-07-30 21:19:34 +0100

    task 0. Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""sets up a simple flask app"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """index route"""
+    return render_template('0-index.html')
